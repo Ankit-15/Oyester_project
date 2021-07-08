bc6a9ffa55de32ec7e113348329c367027e9a01f
Voldemort-1 <piyushd12121@gmail.com>
2021-07-08 16:47:50 +0530

    Deploying

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ app.use((req,res,next)=>{
     next();
 })
 //const y=' || 'mongodb://localhost:27017/wa' ||;
-const dbUrl= 'mongodb+srv://DBpost:NwnXACiUnNotx5uA@cluster0.xynnz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority' || 'mongodb://localhost:27017/wa';
+const dbUrl= process.env.DB_URL || 'mongodb://localhost:27017/wa';
 mongoose.connect(dbUrl, { useCreateIndex: true, useUnifiedTopology: true, useNewUrlParser: true, useFindAndModify: false },(err,client)=>{
     if(!err)console.log("Database Connected!")
 else console.log(err);
